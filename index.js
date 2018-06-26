0ba0b4a49aad7d910ac2bf4de1badd4fba431295
Kathryn Stapleton <kathryn.stapleton777@gmail.com>
2018-06-26 11:58:15 -0400

    trying to add a hidden message

--- index.js
+++ index.js
@@ -13,7 +13,10 @@ function changeHeadingText (){
 function makeInputtedText(){
     const stone= document.querySelector('.stone1').value;
     headingToChange.textContent = stone;
-
+    const regex= /space |mind |time |reality |power |soul/i
+    if (!(stone.includes(regex))){
+        .removeClass('hid')
+    }
     event.preventDefault();
 }
 
