93f6478c0a06081219b003cb814e965ac0777ea7
Kathryn Stapleton <kathryn.stapleton777@gmail.com>
2018-06-27 09:52:59 -0400

    git commit -m "create a list from both fields (2nd field new)"

--- index.js
+++ index.js
@@ -6,12 +6,11 @@ const headingToggleDisappear = document.querySelector('.hid')
 
 let i=10;
 function changeHeadingText (){
-    console.log('i');
     headingToChange.textContent = i;
     if(i > 0) i--;
 }
 
-function makeInputtedText(event){
+function stoneHeadingAndList(event){
     event.preventDefault();  //should be first line
     const f= event.target;
     const stone= f.stone.value;
@@ -21,6 +20,10 @@ function makeInputtedText(event){
     item.textContent = stone;
     const list = document.querySelector('#trove');
     list.appendChild(item);
+    //add second field to list
+    const person = document.createElement('li');
+    person.textContent = f.sender.value;
+    list.appendChild(person);
     //reveal hidden message if doesn't send a stone
     const regex= /.*space.*|.*mind.*|.*time.*|.*reality.*|.*power.*|.*soul.*/i;
     if (!(regex.test(stone))){
@@ -31,4 +34,4 @@ function makeInputtedText(event){
 }
 
 button.addEventListener('click', changeHeadingText);
-form.addEventListener('submit', makeInputtedText);
+form.addEventListener('submit', stoneHeadingAndList);
